c30e615c4f9219cc2c6de542ee4653c18fdf9f2f
nawaz-shah15 <shnwzcs@gmail.com>
2025-11-09 00:23:22 +0530

    "hot fixes"

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const BASE_URL =
   import.meta.env.MODE === "development"
     ? "http://localhost:5001/api"
-    : "https://mern-notetaker-3bn4.onrender.com/api"; 
+    : "https://mern-notetaker-3bn4.onrender.com/api";
 
 const api = axios.create({
   baseURL: BASE_URL,
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  define: {
+    "import.meta.env.MODE": JSON.stringify(process.env.VITE_ENV || "development"),
+  },
 })
+
