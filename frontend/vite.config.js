37d8080634a8b03e728950dbbc757176158fd71e
nawaz-shah15 <shnwzcs@gmail.com>
2025-11-09 00:45:10 +0530

    "mini fixes only"

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,8 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "import.meta.env.MODE": JSON.stringify(process.env.VITE_ENV || "development"),
-  },
 })
 
